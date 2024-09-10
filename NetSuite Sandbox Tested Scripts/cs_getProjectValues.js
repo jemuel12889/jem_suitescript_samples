09a53a5b841d4c77db92b8a57ed78e180eb6493f
jemuel12889 <jem@linkmind.co.jp>
2024-09-10 16:04:28 +0800

    Add files via upload

---
+++ NetSuite Sandbox Tested Scripts/cs_getProjectValues.js
@@ -0,0 +1,36 @@
+/**
+ * @NApiVersion 2.0
+ * @NScriptType ClientScript
+ */
+
+define([], function() {
+    function pageInit(context) {
+        var projectRecord = context.currentRecord;
+
+        var projectName = projectRecord.getValue({
+            fieldId : 'altname'
+        });
+
+        var parent = projectRecord.getValue({
+            fieldId : 'parent'
+        });
+        var parentText = projectRecord.getText({
+            fieldId : 'parent'
+        });
+
+        var expenseType = projectRecord.getValue({
+            fieldId : 'projectexpensetype'
+        });
+        var expenseTypeText = projectRecord.getText({
+            fieldId : 'projectexpensetype'
+        });
+
+        console.log("Project Name: " + projectName);
+        console.log("Project Parent: #" + parent + " " + parentText);
+        console.log("Project Expense Type: " + expenseType + " " + expenseTypeText);
+    }
+
+    return {
+        pageInit : pageInit
+    };
+});
