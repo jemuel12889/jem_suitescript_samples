f5a4d29ebed1f2a3221e0d63783b2c209d0256d5
jemuel12889 <jem@linkmind.co.jp>
2024-10-02 11:06:50 +0800

    Update ss_so_transform_invoice.js

--- NetSuite Sandbox Tested Scripts/Scheduled Scripts/ss_so_transform_invoice.js
+++ NetSuite Sandbox Tested Scripts/Scheduled Scripts/ss_so_transform_invoice.js
@@ -1,67 +1,64 @@
 /**
  * @NApiVersion 2.1
- * @NScriptType ScheduledScript
+ * @NScriptType MapReduceScript
  */
 
 /**
- * This Scheduled Script takes a Saved Search of Sales Orders with the Order Status of "Pending Billing",
+ * This Map/Reduce Script takes a Saved Search of Sales Orders with the Order Status of "Pending Billing",
  * then transforms the resulting Sales Orders to create Invoices for each record.
  */
 define(['N/record', 'N/search'], (record, search) => {
-    function execute(context) {
+    function getInputData(context) {
         // Loading a Saved Search created in the User Interface
         // The Saved Search is for Sales Orders with Order Status of "Pending Billing"
         let soSearch = search.load({
             id : 'customsearch_search_so_pending_billing'
         });
 
-        // Running the Saved Search
-        log.debug('Search', 'Running Sales Order Search');
-        soSearch.run().each((result) => {
-            // Retrieving the Internal ID of the Sales Order in the current iteration
-            let soId = result.getValue({
-                name : 'internalid'
+        // Return the Search as the data to be passed to the Map Stage
+        return soSearch;
+    }
+
+    function map(context) {
+        // Accessing the data from Get Input Stage using the context parameter
+        let soId = context.key;
+        let salesOrderRec = JSON.parse(context.value);
+
+        let customer = salesOrderRec.values.entity.text;
+
+        try {
+            // Using record.transform(options) to transform the current Sales Order using its Internal ID that is retrieved as context.key
+            log.debug('Record Transform', `Transforming Sales Order #${soId} into Invoice for ${customer}.`);
+            let soInvoice = record.transform({
+                fromType : record.Type.SALES_ORDER,
+                fromId : soId,
+                toType : record.Type.INVOICE,
+                isDynamic : true
             });
 
-            // Retrieving the Customer Name from the Sales Order for logging purposes
-            let customer = result.getText({
-                name : 'entity'
+            // Setting the value of the Location field because its value is required for a field in the Item Sublist    
+            soInvoice.setValue({
+                fieldId : 'location',
+                value : 8
             });
 
-            try {
-                // Using record.transform(options) to transform Sales Order into Invoice
-                log.debug('Record Transform', `Creating Invoice for Sales Order #${soId} by Customer: ${customer}`);
-                let soInvoice = record.transform({
-                    fromType : record.Type.SALES_ORDER,
-                    fromId : soId,
-                    toType : record.Type.INVOICE,
-                    isDynamic : true
-                });
+            // Setting the value of the Approval Status field of the newly transformed Invoice as "Approved"
+            soInvoice.setValue({
+                fieldId : 'approvalstatus',
+                value : 2
+            });
 
-                // Setting the value of the Location field because its value is required for a field in the Item Sublist
-                soInvoice.setValue({
-                    fieldId : 'location',
-                    value : 8
-                });
-
-                // Setting the value of the Approval Status field of the newly transformed Invoice as "Approved"
-                soInvoice.setValue({
-                    fieldId : 'approvalstatus',
-                    value : 2
-                });
-
-                // Saving the new Invoice Record
-                let invoiceId = soInvoice.save();
-                log.debug('Invoice Created', `An Invoice has been created for Sales Order #${soId}. Invoice ID is #${invoiceId}`);
-            } catch (error) {
-                log.error('Transform Error', error);
-            }
-
-            return true;
-        });
+            // Saving the new Invoice Record
+            let invoiceId = soInvoice.save();
+            log.debug('Invoice Created', `Created Invoice #${invoiceId} from Sales Order #${soId} by ${customer}.`);
+            
+        } catch (error) {
+            log.error('Transform Failure', error);
+        }
     }
 
     return {
-        execute : execute
+        getInputData : getInputData,
+        map : map
     };
 });
